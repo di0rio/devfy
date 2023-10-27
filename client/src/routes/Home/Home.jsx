60924a0507de5ce592de73b082a599a1e78373c9
di0rio <caua.diorio29@gmail.com>
2023-10-27 17:00:38 -0300

    div home finalizada e livraria iniciada

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -2,7 +2,9 @@ import style from "./Home.module.css";
 
 import Icon from "../../assets/Icon.svg";
 import casa from "../../assets/casa.svg";
-import lupa from "../../assets/lupa.svg"
+import lupa from "../../assets/lupa.svg";
+import livro from "../../assets/livro.svg";
+import mais from "../../assets/mais.svg";
 
 const Home = () => {
   return (
@@ -20,6 +22,14 @@ const Home = () => {
         <div className={style.search}>
           <img src={lupa} alt="Ícone de uma lupa cinza." />
           <a href="#">Search</a>
+        </div>
+      </div>
+      <div className={style.mae}>
+        <div className={style.library}>
+          <img src={livro} alt="" />
+          <p>Library</p>
+          <img className={style.maizi} src={mais} alt="" />
+          <div className={style.playlist}></div>
         </div>
       </div>
     </div>
