1facd3ca0859c89a3b9c7b7dd4680b8257166af9
di0rio <caua.diorio29@gmail.com>
2023-11-05 13:14:11 -0300

    home finalizado, nav finalizado e início do card

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -6,6 +6,11 @@ import lupa from "../../assets/lupa.svg";
 import livro from "../../assets/livro.svg";
 import mais from "../../assets/mais.svg";
 
+import seta1 from "../../assets/seta1.svg";
+import seta2 from "../../assets/seta2.svg";
+
+import mundo from "../../assets/mundo.svg";
+
 const Home = () => {
   return (
     <div className={style.pai}>
@@ -17,19 +22,58 @@ const Home = () => {
         />
         <div className={style.desc}>
           <img src={casa} alt="Ícone de uma casa branca." />
-          <a href="#">Home</a>
+          <a href="#">Início</a>
         </div>
         <div className={style.search}>
           <img src={lupa} alt="Ícone de uma lupa cinza." />
-          <a href="#">Search</a>
+          <a href="#">Buscar</a>
         </div>
+      </div>
+      <div className={style.acount}>
+        <div className={style.setas}>
+          <img src={seta1} alt="seta para o lado esquerdo." />
+          <img src={seta2} alt="seta para o lado direito." />
+        </div>
+        <div>
+          <a href="#">Inscrever-se</a>
+          <button className={style.entrar}>Entrar</button>
+        </div>
+      </div>
+      <div className={style.card}>
+        <div></div>
       </div>
       <div className={style.mae}>
         <div className={style.library}>
-          <img src={livro} alt="" />
-          <p>Library</p>
-          <img className={style.maizi} src={mais} alt="" />
+          <img src={livro} alt="ícone de livro cinza." />
+          <p>Sua Biblioteca</p>
+          <img className={style.maizi} src={mais} alt="botão de mais." />
           <div className={style.playlist}></div>
+        </div>
+        <div className={style.filhos}>
+          <div className={style.playliste}>
+            <h4>Crie sua primeira playlist</h4>
+            <p>É fácil, vamos te ajudar.</p>
+            <div>
+              <button className={style.create}>Criar playlist</button>
+            </div>
+          </div>
+        </div>
+        <div className={style.filhoss}>
+          <div className={style.podcast}>
+            <h4>Que tal seguir um podcast novo?</h4>
+            <p>Avisaremos você sobre novos episódios.</p>
+            <button className={style.createP}>Explore podcasts</button>
+          </div>
+        </div>
+        <div className={style.avos}>
+          <div className={style.tio}>
+            <a href="#">Cookies</a>
+          </div>
+          <div className={style.tia}>
+            <button className={style.btnL}>
+              <img src={mundo} alt="ícone de um mundo cinza." /> Português
+            </button>
+          </div>
         </div>
       </div>
     </div>
